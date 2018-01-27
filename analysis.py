e0dd14c6f8e1cccf99c3e525f9ab130db5b1712c
Neel Kant <kantneel@gmail.com>
2018-01-26 22:12:06 -0800

    preliminary work

---
+++ analysis.py
@@ -0,0 +1,97 @@
+
+# {"code": 
+# 		{"info": {"candidates": [["DEF", "run", "m(", "move", "pickMarker", "m)"]], "trees_checked": 1},
+# 		 "code_sequence": [],
+# 		 "code_tree": []
+# 		},
+#  "stats": {"result-none": 0, "runtime-exception": 0, "syntax-error": 0, "exceptions": 0, "total": 6, "correct": 0}, 
+#  "example": 
+# 	    {"code": ["DEF", "run", "m(", "move", "putMarker", "m)"],
+# 	     "guid": "b9637eed2a253a87", 
+# 	     "examples": [{"out": [542, 1620, 1621, 1622, 1623, 1624, 1638, 1642, 1656, 1660, 1674, 1678, 1692, 1696, 1710, 1714, 1728, 1732, 1746, 1750, 1764, 1768, 1782, 1786, 1800, 1804, 1818, 1822, 1836, 1840, 1854, 1858, 1872, 1876, 1890, 1891, 1892, 1893, 1894, 1999, 2162, 4232], "in": [541, 1620, 1621, 1622, 1623, 1624, 1638, 1642, 1656, 1660, 1674, 1678, 1692, 1696, 1710, 1714, 1728, 1732, 1746, 1750, 1764, 1768, 1782, 1786, 1800, 1804, 1818, 1822, 1836, 1840, 1854, 1858, 1872, 1876, 1890, 1891, 1892, 1893, 1894, 1999, 4232]}, {"out": [370, 1620, 1621, 1622, 1623, 1624, 1625, 1626, 1627, 1628, 1629, 1630, 1631, 1632, 1633, 1634, 1635, 1636, 1637, 1638, 1655, 1656, 1673, 1674, 1675, 1676, 1677, 1678, 1679, 1680, 1681, 1682, 1683, 1684, 1685, 1686, 1687, 1688, 1689, 1690, 1691, 1968, 1972, 1983, 1984, 1990, 3598], "in": [369, 1620, 1621, 1622, 1623, 1624, 1625, 1626, 1627, 1628, 1629, 1630, 1631, 1632, 1633, 1634, 1635, 1636, 1637, 1638, 1655, 1656, 1673, 1674, 1675, 1676, 1677, 1678, 1679, 1680, 1681, 1682, 1683, 1684, 1685, 1686, 1687, 1688, 1689, 1690, 1691, 1968, 1972, 1983, 1984, 3598]}, {"out": [704, 1620, 1621, 1622, 1623, 1624, 1625, 1626, 1627, 1628, 1629, 1630, 1631, 1632, 1638, 1650, 1656, 1668, 1674, 1686, 1692, 1704, 1710, 1711, 1712, 1713, 1714, 1715, 1716, 1717, 1718, 1719, 1720, 1721, 1722, 1969, 2324, 2653, 3284, 3933, 3935], "in": [722, 1620, 1621, 1622, 1623, 1624, 1625, 1626, 1627, 1628, 1629, 1630, 1631, 1632, 1638, 1650, 1656, 1668, 1674, 1686, 1692, 1704, 1710, 1711, 1712, 1713, 1714, 1715, 1716, 1717, 1718, 1719, 1720, 1721, 1722, 1969, 2000, 2653, 3284, 3933, 3935]}, {"out": [344, 1620, 1621, 1622, 1623, 1624, 1625, 1626, 1627, 1628, 1629, 1630, 1638, 1648, 1656, 1666, 1674, 1684, 1692, 1702, 1710, 1720, 1728, 1738, 1746, 1756, 1764, 1765, 1766, 1767, 1768, 1769, 1770, 1771, 1772, 1773, 1774, 1964, 1987, 2017, 2036, 2058], "in": [343, 1620, 1621, 1622, 1623, 1624, 1625, 1626, 1627, 1628, 1629, 1630, 1638, 1648, 1656, 1666, 1674, 1684, 1692, 1702, 1710, 1720, 1728, 1738, 1746, 1756, 1764, 1765, 1766, 1767, 1768, 1769, 1770, 1771, 1772, 1773, 1774, 1987, 2017, 2036, 2058]}, {"out": [44, 1620, 1621, 1622, 1623, 1624, 1625, 1626, 1627, 1628, 1629, 1630, 1631, 1638, 1649, 1656, 1667, 1674, 1685, 1692, 1693, 1694, 1695, 1696, 1697, 1698, 1699, 1700, 1701, 1702, 1703, 1988, 1989, 2002, 2324, 2612], "in": [26, 1620, 1621, 1622, 1623, 1624, 1625, 1626, 1627, 1628, 1629, 1630, 1631, 1638, 1649, 1656, 1667, 1674, 1685, 1692, 1693, 1694, 1695, 1696, 1697, 1698, 1699, 1700, 1701, 1702, 1703, 1989, 2002, 2324, 2612]}, {"out": [63, 1620, 1621, 1622, 1623, 1624, 1625, 1626, 1627, 1628, 1629, 1630, 1631, 1632, 1638, 1650, 1656, 1668, 1674, 1686, 1692, 1704, 1710, 1722, 1728, 1740, 1746, 1747, 1748, 1749, 1750, 1751, 1752, 1753, 1754, 1755, 1756, 1757, 1758, 2007, 2054, 2061, 3314, 3606, 3949, 4293, 4309], "in": [45, 1620, 1621, 1622, 1623, 1624, 1625, 1626, 1627, 1628, 1629, 1630, 1631, 1632, 1638, 1650, 1656, 1668, 1674, 1686, 1692, 1704, 1710, 1722, 1728, 1740, 1746, 1747, 1748, 1749, 1750, 1751, 1752, 1753, 1754, 1755, 1756, 1757, 1758, 2054, 2061, 3314, 3606, 3949, 4293, 4309]}]
+# 		}
+# }
+
+import json
+import numpy as np
+import itertools
+
+map_features = {"height" : 0, "width" : 0, "num_int_walls" : 0, 
+				"total_markers" : 0, "max_markers" : 0, "spots_markers" : 0}
+
+delta_features = {"total_markers" : 0, "max_markers" : 0, "spots_markers" : 0, 
+				  "robot_height" : 0, "robot_width" : 0}
+
+def featurize(episode):
+	# currently assumes a single candidate/trial. 
+	correct = int(episode['stats']['correct'] == 6) # whether the model got it right
+
+	model_out = featurize_code(episode['code']['info']['candidates'][0])
+	target_code = featurize_code(episode['example']['code'])
+
+	maps = episode['example']['examples']
+	in_maps = list(itertools.chain.from_iterable([featurize_io(m['in']) for m in maps]))
+	out_maps = list(itertools.chain.from_iterable([featurize_io(m['out']) for m in maps]))
+
+	return model_out + target_code + in_maps + out_maps, correct
+
+
+def featurize_io(map_indices):
+	flat = np.zeros(15 * 18 * 18)
+	flat[map_indices] = 1
+	one_hot = flat.reshape((15, 18, 18))
+
+	height = np.sum(one_hot[5], axis=0)[0] - 2
+	width = np.sum(one_hot[5], axis=1)[0] - 2
+
+	num_int_walls = np.sum(one_hot[4])
+
+	total_markers = 0 
+	max_markers = 0
+	spots_markers = 0
+	for i in range(1, 10):
+		i_markers = np.sum(one_hot[i + 5])
+		if i_markers > 0:
+			max_markers = i
+			spots_markers += i_markers
+			total_markers += i * i_markers
+
+	return [height, width, num_int_walls, total_markers, max_markers, spots_markers]
+
+def featurize_code(code_string):
+	length = len(code_string)
+	clause_show = lambda letter : [int(len(s) == 2 and letter in s) for s in code_string]
+	clause_sum = lambda letter : sum(clause_show(letter)) / 2
+
+	# conds = clause_sum('c') sum of i, w. 
+	ifs = clause_sum('i')
+	elses = clause_sum('e')
+	whiles = clause_sum('w')
+	repeats = clause_sum('r')
+
+	return [length, ifs, elses, whiles, repeats]
+
+if __name__ == '__main__':
+	FILENAME = "report-val.json"
+
+	# features: 12 * (height, width, num_int_walls, {total, max, spots}_markers) = 72 +
+	# 2 * (length, ifs, elses, whiles, repeats) = 82
+	NUM_FEATURES = 82
+
+	f = open(FILENAME)
+	overall_stats = json.loads(f.readline()) # ex {"syntax-error": 0, "total": 192, "correct": 3, "runtime-exception": 0}
+	num_records = overall_stats['total']
+
+	data_array = np.zeros((num_records, NUM_FEATURES))
+	label_array = np.zeros(num_records)
+
+	for i in range(num_records):
+		episode_dict = json.loads(f.readline())
+		featurized, label = featurize(episode_dict)
+
+		data_array[i] = featurized
+		label_array[i] = label
+
+	np.savetxt('data.csv', data_array, delimiter=',')
+	np.savetxt('labels.csv', label_array, delimiter=',')
+
