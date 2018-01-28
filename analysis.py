d0abc160f01e53f5088e75efcfc2c7e6dc33952c
Neel Kant <kantneel@gmail.com>
2018-01-28 15:31:58 -0800

    feauturization and modeling done

--- analysis.py
+++ analysis.py
@@ -16,35 +16,72 @@ import json
 import numpy as np
 import itertools
 
-map_features = {"height" : 0, "width" : 0, "num_int_walls" : 0, 
-				"total_markers" : 0, "max_markers" : 0, "spots_markers" : 0}
-
-delta_features = {"total_markers" : 0, "max_markers" : 0, "spots_markers" : 0, 
-				  "robot_height" : 0, "robot_width" : 0}
-
 def featurize(episode):
-	# currently assumes a single candidate/trial. 
+	"""
+	Input: a dictionary which represents an evaluated point in the validation set. 
+	Output: a list of features about the evaluated point.
+ 
+	Currently assumes a single candidate code sequence per trial.
+	"""
+
 	correct = int(episode['stats']['correct'] == 6) # whether the model got it right
 
-	model_out = featurize_code(episode['code']['info']['candidates'][0])
-	target_code = featurize_code(episode['example']['code'])
+	# model_out = featurize_code(episode['code']['info']['candidates'][0])
+	featurized_code = featurize_code(episode['example']['code']) # 18 features
 
-	maps = episode['example']['examples']
-	in_maps = list(itertools.chain.from_iterable([featurize_io(m['in']) for m in maps]))
-	out_maps = list(itertools.chain.from_iterable([featurize_io(m['out']) for m in maps]))
+	maps = episode['example']['examples'] # list of all of the i/o pairs
 
-	return model_out + target_code + in_maps + out_maps, correct
+	in_featurized = [featurize_io(m['in']) for m in maps] # 6 x 7 = 42 features
+	delta_featurized = [featurize_delta(m['in'], m['out']) for m in maps] # 6 x 6 = 36 features
 
+	featurized_io = [t[0] + t[1] for t in zip(in_featurized, delta_featurized)]
+	flat_featurized_io = list(itertools.chain.from_iterable(featurized_io))
+
+	return featurized_code + flat_featurized_io, correct
+
+def get_one_hot(indices):
+	"""
+	Input: a list of indices which refer to positions marked "True" in a Karel grid.
+	Output: a one-hot numpy array for the grid.
+	"""
+	grid = np.zeros((15, 18, 18))
+	grid.ravel()[indices] = 1
+
+	return grid
 
 def featurize_io(map_indices):
-	flat = np.zeros(15 * 18 * 18)
-	flat[map_indices] = 1
-	one_hot = flat.reshape((15, 18, 18))
+	"""
+	Input: a list of indices which refer to positions marked "True" in a Karel grid.
+	Output: a list of features about the input grid.
 
-	height = np.sum(one_hot[5], axis=0)[0] - 2
-	width = np.sum(one_hot[5], axis=1)[0] - 2
+	Current features: perimeter, area, number of interior walls,
+	max number of markers in a spot, number of spots with markers, total number of markers
+	6 total.
+	"""
+	one_hot = get_one_hot(map_indices)
+
+	height = int(np.sum(one_hot[5], axis=0)[0] - 2)
+	width = int(np.sum(one_hot[5], axis=1)[0] - 2)
+
+	perimiter = height + width
+	area = height * width
 
 	num_int_walls = np.sum(one_hot[4])
+
+	shared_edges = 0
+	for i in range(1, height + 1):
+		for j in range(1, width + 1):
+			if one_hot[4, i, j] == 1:
+
+				# sum up the number of edges that are against another wall  
+				up_down = one_hot[4, i + 1, j] + one_hot[4, i - 1, j]
+				up_down += int(i == 1 or i == height)
+				left_right = one_hot[4, i, j + 1] + one_hot[4, i, j - 1]
+				left_right += int(i == 1 or i == width)
+
+				shared_edges += up_down + left_right
+
+	exposed_edges = 4 * num_int_walls - shared_edges # tells you something about interior walls...
 
 	total_markers = 0 
 	max_markers = 0
@@ -56,27 +93,103 @@ def featurize_io(map_indices):
 			spots_markers += i_markers
 			total_markers += i * i_markers
 
-	return [height, width, num_int_walls, total_markers, max_markers, spots_markers]
+	return [perimiter, area, num_int_walls, exposed_edges, total_markers, max_markers, spots_markers]
+
+def featurize_delta(map_in, map_out):
+	"""
+	Input: two lists of indices (as per featurize_io), for in and out Karel maps. 
+	Output: a list of features about the difference between the two maps.
+
+	Current features: changes in marker stats (as per featurize_io), 
+	whether Karel is facing in the same direction, the change in Karel's position (x, y)
+	6 total.
+	"""
+	in_rep = get_one_hot(map_in)
+	out_rep = get_one_hot(map_out)
+
+	in_features = featurize_io(map_in)
+	out_features = featurize_io(map_out)
+
+	marker_diffs = [in_features[i] - out_features[i] for i in range(3, 6)]
+
+	# find direction hero is facing (in, out)
+	in_facing = np.argmax([np.max(in_rep[i]) for i in range(3)])
+	out_facing = np.argmax([np.max(out_rep[i]) for i in range(3)])
+
+	# find coordinates of hero (in, out)
+	in_coords = np.unravel_index(np.argmax(in_rep[in_facing]), (18, 18))
+	out_coords = np.unravel_index(np.argmax(out_rep[out_facing]), (18, 18))
+
+
+	face_change = int(in_facing == out_facing)
+	movedx = np.abs(in_coords[0] - out_coords[0]) 
+	movedy = np.abs(in_coords[1] - out_coords[1])
+
+	return [face_change, movedx, movedy] + marker_diffs
+
 
 def featurize_code(code_string):
+	"""
+	Input: a list of tokens corresponding to the target code sequence
+	Output: a list of features about the code.
+
+	Current features: program length, a measure of program depth, 
+	and a bag of words representation. 
+	18 total.
+	"""
+
 	length = len(code_string)
-	clause_show = lambda letter : [int(len(s) == 2 and letter in s) for s in code_string]
-	clause_sum = lambda letter : sum(clause_show(letter)) / 2
 
-	# conds = clause_sum('c') sum of i, w. 
-	ifs = clause_sum('i')
-	elses = clause_sum('e')
-	whiles = clause_sum('w')
-	repeats = clause_sum('r')
+	# no case-switch lol
+	# bag of words 
+	token_dict = {
+		'move'				: 0,
+		'putMarker' 		: 1, 
+		'pickMarker' 		: 2, 
+		'i('				: 3, 
+		'e('				: 4, 
+		'w('				: 5, 
+		'r('				: 6,  
+		'frontIsClear'		: 7,
+		'leftIsClear'		: 8, 
+		'rightIsClear'  	: 9, 
+		'not'				: 10, 
+		'turnLeft'			: 11, 
+		'turnRight'			: 12, 
+		'markersPresent'	: 13, 
+		'noMarkersPresent'	: 14
+		# Repeats (R=*)		: 15	
+	}
+	
+	bag_of_words = [0 for i in range(16)]
+	depth_counter = [0 for i in range(length)] 
 
-	return [length, ifs, elses, whiles, repeats]
+	for i, token in enumerate(code_string):
+		if token in token_dict:
+			bag_of_words[token_dict[token]] += 1
+		elif 'R=' in token:
+			bag_of_words[15] += 1
+		
+		if '(' in token and 'm' not in token:
+			depth_counter[i] = depth_counter[i - 1] + 1
+		elif ')' in token and 'm' not in token:
+			depth_counter[i] = depth_counter[i - 1] - 1
+		else:
+			depth_counter[i] = depth_counter[max(i - 1, 0)]
+
+	program_depth = sum(depth_counter)
+
+	return [length, program_depth] + bag_of_words
 
 if __name__ == '__main__':
-	FILENAME = "report-val.json"
 
-	# features: 12 * (height, width, num_int_walls, {total, max, spots}_markers) = 72 +
-	# 2 * (length, ifs, elses, whiles, repeats) = 82
-	NUM_FEATURES = 82
+	# Main function: 
+	# 1. read data from json.
+	# 2. featurize it. 
+	# 3. save it into numpy arrays. 
+
+	FILENAME = "report-val.json"
+	NUM_FEATURES = 96 # 42 + 36 + 18 = 96 features (check out featurize for details).
 
 	f = open(FILENAME)
 	overall_stats = json.loads(f.readline()) # ex {"syntax-error": 0, "total": 192, "correct": 3, "runtime-exception": 0}
@@ -92,6 +205,6 @@ if __name__ == '__main__':
 		data_array[i] = featurized
 		label_array[i] = label
 
-	np.savetxt('data.csv', data_array, delimiter=',')
-	np.savetxt('labels.csv', label_array, delimiter=',')
+	np.savetxt('data.csv', data_array.astype(int), delimiter=',')
+	np.savetxt('labels.csv', label_array.astype(int), delimiter=',')
 
